1904a311c288b3195c99a9e8d770468b3964bd40
Wesley Brooks <brooks.wesley@gmail.com>
2013-12-12 16:23:24 -0600

    in midst of wisc-viz session

--- gridSVG/script.r
+++ gridSVG/script.r
@@ -3,12 +3,24 @@ require(rjson)
 require(gridSVG)
 
 data(cars)
-
-p1 = ggplot(cars) + aes(speed,dist) + geom_point()
+p1 = ggplot(cars) + aes(speed,dist) + geom_point() + coord_cartesian(xlim=c(0,30), ylim=c(0,125))
 print(p1)
 grid.export("cars.svg", addClasses=TRUE)
+
+#write the data for the plot:
+jj = rjson::toJSON(apply(p1$data, MARGIN=1, FUN=function(x) {return(list(x))}))
+sink("cars.json")
+cat(jj)
+sink()
+
 
 data(longley)
 p2 = ggplot(longley) + aes(Population,GNP) + geom_point()
 print(p2)
 grid.export("gnp.svg", addClasses=TRUE)
+
+#write the data for the plot:
+jj = rjson::toJSON(apply(p2$data, MARGIN=1, FUN=function(x) {return(list(x))}))
+sink("gnp.json")
+cat(jj)
+sink()
