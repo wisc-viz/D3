b2059c3f8a398da267f95b7c2af683e107340389
Wesley Brooks <brooks.wesley@gmail.com>
2013-12-12 14:57:10 -0600

    switching machines

---
+++ gridSVG/cars.svg.mappings.js
@@ -0,0 +1,204 @@
+var gridSVGMappings = {
+ "vps": {
+ "layout": {
+ "suffix":      1,
+"selector": "#layout\\.1",
+"xpath": "//*[@id='layout.1']" 
+},
+"layout::background.1-5-6-1": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a background\\.1-5-6-1\\.1",
+"xpath": "//*[@id='layout::background.1-5-6-1.1']" 
+},
+"layout::spacer.4-3-4-3": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a spacer\\.4-3-4-3\\.1",
+"xpath": "//*[@id='layout::spacer.4-3-4-3.1']" 
+},
+"layout::panel.3-4-3-4": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a panel\\.3-4-3-4\\.1",
+"xpath": "//*[@id='layout::panel.3-4-3-4.1']" 
+},
+"layout::axis-l.3-3-3-3": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-l\\.3-3-3-3\\.1",
+"xpath": "//*[@id='layout::axis-l.3-3-3-3.1']" 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.158": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-l\\.3-3-3-3\\3a \\3a GRID\\.VP\\.158\\.1",
+"xpath": "//*[@id='layout::axis-l.3-3-3-3::GRID.VP.158.1']" 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.158::axis": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-l\\.3-3-3-3\\3a \\3a GRID\\.VP\\.158\\3a \\3a axis\\.1",
+"xpath": "//*[@id='layout::axis-l.3-3-3-3::GRID.VP.158::axis.1']" 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.158::axis::axis.1-1-1-1": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-l\\.3-3-3-3\\3a \\3a GRID\\.VP\\.158\\3a \\3a axis\\3a \\3a axis\\.1-1-1-1\\.1",
+"xpath": "//*[@id='layout::axis-l.3-3-3-3::GRID.VP.158::axis::axis.1-1-1-1.1']" 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.158::axis::axis.1-2-1-2": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-l\\.3-3-3-3\\3a \\3a GRID\\.VP\\.158\\3a \\3a axis\\3a \\3a axis\\.1-2-1-2\\.1",
+"xpath": "//*[@id='layout::axis-l.3-3-3-3::GRID.VP.158::axis::axis.1-2-1-2.1']" 
+},
+"layout::axis-b.4-4-4-4": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-b\\.4-4-4-4\\.1",
+"xpath": "//*[@id='layout::axis-b.4-4-4-4.1']" 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.157": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-b\\.4-4-4-4\\3a \\3a GRID\\.VP\\.157\\.1",
+"xpath": "//*[@id='layout::axis-b.4-4-4-4::GRID.VP.157.1']" 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.157::axis": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-b\\.4-4-4-4\\3a \\3a GRID\\.VP\\.157\\3a \\3a axis\\.1",
+"xpath": "//*[@id='layout::axis-b.4-4-4-4::GRID.VP.157::axis.1']" 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.157::axis::axis.1-1-1-1": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-b\\.4-4-4-4\\3a \\3a GRID\\.VP\\.157\\3a \\3a axis\\3a \\3a axis\\.1-1-1-1\\.1",
+"xpath": "//*[@id='layout::axis-b.4-4-4-4::GRID.VP.157::axis::axis.1-1-1-1.1']" 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.157::axis::axis.2-1-2-1": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a axis-b\\.4-4-4-4\\3a \\3a GRID\\.VP\\.157\\3a \\3a axis\\3a \\3a axis\\.2-1-2-1\\.1",
+"xpath": "//*[@id='layout::axis-b.4-4-4-4::GRID.VP.157::axis::axis.2-1-2-1.1']" 
+},
+"layout::xlab.5-4-5-4": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a xlab\\.5-4-5-4\\.1",
+"xpath": "//*[@id='layout::xlab.5-4-5-4.1']" 
+},
+"layout::ylab.3-2-3-2": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a ylab\\.3-2-3-2\\.1",
+"xpath": "//*[@id='layout::ylab.3-2-3-2.1']" 
+},
+"layout::title.2-4-2-4": {
+ "suffix":      1,
+"selector": "#layout\\3a \\3a title\\.2-4-2-4\\.1",
+"xpath": "//*[@id='layout::title.2-4-2-4.1']" 
+} 
+},
+"grobs": {
+ "GRID.gTableParent.232": {
+ "suffix":      1,
+"selector": "#GRID\\.gTableParent\\.232\\.1",
+"xpath": "//*[@id='GRID.gTableParent.232.1']" 
+},
+"background.1-5-6-1": {
+ "suffix":      1,
+"selector": "#background\\.1-5-6-1\\.1",
+"xpath": "//*[@id='background.1-5-6-1.1']" 
+},
+"panel.3-4-3-4": {
+ "suffix":      1,
+"selector": "#panel\\.3-4-3-4\\.1",
+"xpath": "//*[@id='panel.3-4-3-4.1']" 
+},
+"grill.gTree.210": {
+ "suffix":      1,
+"selector": "#grill\\.gTree\\.210\\.1",
+"xpath": "//*[@id='grill.gTree.210.1']" 
+},
+"panel.background.rect.201": {
+ "suffix":      1,
+"selector": "#panel\\.background\\.rect\\.201\\.1",
+"xpath": "//*[@id='panel.background.rect.201.1']" 
+},
+"panel.grid.minor.y.polyline.203": {
+ "suffix":      1,
+"selector": "#panel\\.grid\\.minor\\.y\\.polyline\\.203\\.1",
+"xpath": "//*[@id='panel.grid.minor.y.polyline.203.1']" 
+},
+"panel.grid.minor.x.polyline.205": {
+ "suffix":      1,
+"selector": "#panel\\.grid\\.minor\\.x\\.polyline\\.205\\.1",
+"xpath": "//*[@id='panel.grid.minor.x.polyline.205.1']" 
+},
+"panel.grid.major.y.polyline.207": {
+ "suffix":      1,
+"selector": "#panel\\.grid\\.major\\.y\\.polyline\\.207\\.1",
+"xpath": "//*[@id='panel.grid.major.y.polyline.207.1']" 
+},
+"panel.grid.major.x.polyline.209": {
+ "suffix":      1,
+"selector": "#panel\\.grid\\.major\\.x\\.polyline\\.209\\.1",
+"xpath": "//*[@id='panel.grid.major.x.polyline.209.1']" 
+},
+"geom_point.points.194": {
+ "suffix":      1,
+"selector": "#geom_point\\.points\\.194\\.1",
+"xpath": "//*[@id='geom_point.points.194.1']" 
+},
+"ablines.gTree.197": {
+ "suffix":      1,
+"selector": "#ablines\\.gTree\\.197\\.1",
+"xpath": "//*[@id='ablines.gTree.197.1']" 
+},
+"GRID.segments.195": {
+ "suffix":      1,
+"selector": "#GRID\\.segments\\.195\\.1",
+"xpath": "//*[@id='GRID.segments.195.1']" 
+},
+"axis-l.3-3-3-3": {
+ "suffix":      1,
+"selector": "#axis-l\\.3-3-3-3\\.1",
+"xpath": "//*[@id='axis-l.3-3-3-3.1']" 
+},
+"GRID.gTableParent.262": {
+ "suffix":      1,
+"selector": "#GRID\\.gTableParent\\.262\\.1",
+"xpath": "//*[@id='GRID.gTableParent.262.1']" 
+},
+"axis.1-1-1-1": {
+ "suffix": [      1,      2 ],
+"selector": [ "#axis\\.1-1-1-1\\.1", "#axis\\.1-1-1-1\\.2" ],
+"xpath": [ "//*[@id='axis.1-1-1-1.1']", "//*[@id='axis.1-1-1-1.2']" ] 
+},
+"axis.1-2-1-2": {
+ "suffix":      1,
+"selector": "#axis\\.1-2-1-2\\.1",
+"xpath": "//*[@id='axis.1-2-1-2.1']" 
+},
+"axis-b.4-4-4-4": {
+ "suffix":      1,
+"selector": "#axis-b\\.4-4-4-4\\.1",
+"xpath": "//*[@id='axis-b.4-4-4-4.1']" 
+},
+"GRID.gTableParent.270": {
+ "suffix":      1,
+"selector": "#GRID\\.gTableParent\\.270\\.1",
+"xpath": "//*[@id='GRID.gTableParent.270.1']" 
+},
+"axis.2-1-2-1": {
+ "suffix":      1,
+"selector": "#axis\\.2-1-2-1\\.1",
+"xpath": "//*[@id='axis.2-1-2-1.1']" 
+},
+"xlab.5-4-5-4": {
+ "suffix":      1,
+"selector": "#xlab\\.5-4-5-4\\.1",
+"xpath": "//*[@id='xlab.5-4-5-4.1']" 
+},
+"ylab.3-2-3-2": {
+ "suffix":      1,
+"selector": "#ylab\\.3-2-3-2\\.1",
+"xpath": "//*[@id='ylab.3-2-3-2.1']" 
+},
+"title.2-4-2-4": {
+ "suffix":      1,
+"selector": "#title\\.2-4-2-4\\.1",
+"xpath": "//*[@id='title.2-4-2-4.1']" 
+} 
+},
+"refs": null,
+"id.sep": ".",
+"prefix": "" 
+};
---
+++ gridSVG/gnp.svg.coords.js
@@ -0,0 +1,182 @@
+var gridSVGCoords = {
+ "ROOT": {
+ "x":      0,
+"y":      0,
+"width":    504,
+"height":    504,
+"angle":      0,
+"xscale": [      0,    504 ],
+"yscale": [    504,      0 ],
+"inch":     72 
+},
+"layout.1": {
+ "x":      0,
+"y":      0,
+"width":    504,
+"height":    504,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::background.1-5-6-1.1": {
+ "x":      0,
+"y":     -0,
+"width":    504,
+"height":    504,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::spacer.4-3-4-3.1": {
+ "x":  22.99,
+"y":  22.99,
+"width":   23.1,
+"height":  13.96,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::panel.3-4-3-4.1": {
+ "x":  46.09,
+"y":  36.95,
+"width": 443.51,
+"height": 452.65,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-l.3-3-3-3.1": {
+ "x":  22.99,
+"y":  36.95,
+"width":   23.1,
+"height": 452.65,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.310.1": {
+ "x":  22.99,
+"y":  36.95,
+"width":   23.1,
+"height": 452.65,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.310::axis.1": {
+ "x":  22.99,
+"y":  36.95,
+"width":   23.1,
+"height": 452.65,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.310::axis::axis.1-1-1-1.1": {
+ "x":  22.99,
+"y":  36.95,
+"width":  16.02,
+"height": 452.65,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-l.3-3-3-3::GRID.VP.310::axis::axis.1-2-1-2.1": {
+ "x":  39.01,
+"y":  36.95,
+"width":   7.09,
+"height": 452.65,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-b.4-4-4-4.1": {
+ "x":  46.09,
+"y":  22.99,
+"width": 443.51,
+"height":  13.96,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.309.1": {
+ "x":  46.09,
+"y":  22.99,
+"width": 443.51,
+"height":  13.96,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.309::axis.1": {
+ "x":  46.09,
+"y":  22.99,
+"width": 443.51,
+"height":  13.96,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.309::axis::axis.1-1-1-1.1": {
+ "x":  46.09,
+"y":  29.86,
+"width": 443.51,
+"height":   7.09,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::axis-b.4-4-4-4::GRID.VP.309::axis::axis.2-1-2-1.1": {
+ "x":  46.09,
+"y":  22.99,
+"width": 443.51,
+"height":   6.87,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::xlab.5-4-5-4.1": {
+ "x":  46.09,
+"y":    7.2,
+"width": 443.51,
+"height":  15.79,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::ylab.3-2-3-2.1": {
+ "x":    7.2,
+"y":  36.95,
+"width":  15.79,
+"height": 452.65,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+},
+"layout::title.2-4-2-4.1": {
+ "x":  46.09,
+"y":  489.6,
+"width": 443.51,
+"height":      0,
+"angle":      0,
+"xscale": [      0,      1 ],
+"yscale": [      0,      1 ],
+"inch":     72 
+} 
+};
